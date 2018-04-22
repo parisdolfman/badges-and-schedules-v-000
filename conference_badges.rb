32d5021e0e0d49e30292f636e41424a8c91a6694
Paris Dolfman <parisdolfman@gmail.com>
2018-04-22 21:17:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,10 @@ def badge_maker(name)
  phrase = "Hello, my name is #{name}."
  phrase
 end
+
+def batch_badge_creator(attendees)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  each.do |name|
+  puts "Hello, my name is #{name}."
+end 
+end
